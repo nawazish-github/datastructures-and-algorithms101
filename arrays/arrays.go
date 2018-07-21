7c59df4cfad0d76deda6584c310d59aedad3ca57
mnawaz <mnawaz@thoughtworks.com>
2018-07-21 22:41:13 +0530

    Provide Add method with elastic behavior

---
+++ arrays/arrays.go
@@ -0,0 +1,40 @@
+package arrays
+
+const initCap = 10
+
+//DynamicArrays represents an elastic array
+type DynamicArrays struct {
+	Size     int           //Size is the current number of elements in the DynamicArray
+	Capacity int           //Capacity total number of elements which can be accomodated into the DynamicArray
+	Data     []interface{} //Data is the underlying storage for the DynamicArray
+}
+
+func NewArray() *DynamicArrays {
+	return &DynamicArrays{
+		Size:     0,
+		Capacity: initCap,
+		Data:     make([]interface{}, initCap),
+	}
+}
+
+//Add adds the given element to the end of the DynamicArray
+//If there is not enough space to add, it grows itself.
+func (d *DynamicArrays) Add(elem interface{}) {
+	if d.Size == d.Capacity {
+		d.Size = 0
+		newCap := d.Capacity * 2
+		tempData := make([]interface{}, newCap)
+		for idx, val := range d.Data {
+			tempData[idx] = val
+			d.Size = d.Size + 1
+		}
+		d.Capacity = newCap
+		d.Data = tempData
+		d.Data[d.Size] = elem
+		d.Size = d.Size + 1
+		return
+	}
+
+	d.Data[d.Size] = elem
+	d.Size = d.Size + 1
+}
