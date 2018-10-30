e6fa08516fe55083b9aa0f3bde0d3f2b0f066034
mnawaz <mnawaz@thoughtworks.com>
2018-10-31 02:11:52 +0530

    Knapsack refactored

--- knapsack/knapsack.go
+++ knapsack/knapsack.go
@@ -4,26 +4,22 @@ import "fmt"
 
 var val [6]int
 var wt [6]int
-var mat [7][11]int
 
+//C is target capacity
 const C = 10
+
+var mat [len(wt) + 1][C + 1]int //matrix mat[7][11] zeroth row and zerowth column used as dummy initialization
 
 func main() {
 	val = [6]int{2, 3, 8, 10, 12, 13}
 	wt = [6]int{1, 2, 3, 4, 5, 6}
 
-	//idx := 0
-	for i := 1; i <= 6; i++ { //rows
+	for i := 1; i <= len(wt); i++ { //rows
 		currWeight := wt[i-1]
 		currValue := val[i-1]
 
-		for j := 1; j <= 10; j++ { //columns
+		for j := 1; j <= C; j++ { //columns
 			if currWeight == j {
-				//maxVal := max(currValue+recurr(col, prevRow), mat[i-1][j])
-				if i == 0 {
-					mat[i][j] = 0
-					continue
-				}
 				maxVal := max(currValue+recurr(i-1, j-i), mat[i-1][j])
 				mat[i][j] = maxVal
 				continue
@@ -31,26 +27,21 @@ func main() {
 
 			if currWeight < j {
 				optVal := max(currValue+recurr(i-1, j-currWeight), mat[i-1][j])
-				//prevOptVal := mat[i-1][j]
 				mat[i][j] = optVal
 				continue
 			}
 
 			if currWeight > j {
-				//maxVal := max(currValue+recurr(i-1, i-j), mat[i-1][j])
 				optVal := mat[i-1][j]
 				mat[i][j] = optVal
 			}
 		}
 	}
 
-	fmt.Println(mat)
+	fmt.Println("Max value in the knapsack: ", mat[6][10])
 }
 
 func recurr(prevRow, prevCol int) int {
-	if prevRow < 0 {
-		return 0
-	}
 	return mat[prevRow][prevCol]
 }
 
